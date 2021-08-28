c5747fd3461e29d181d88a663b8bf1d51d9bd2a4
Arthur <arthurhavlicek@gmail.com>
2021-08-28 13:54:30 +0200

    Toy with rabbitmq

--- server.py
+++ server.py
@@ -19,6 +19,16 @@ import ssl
 from collections import defaultdict
 import yaml
 import sys
+import pika
+
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+channel.queue_declare(queue='hello')
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body=b'Hello World!')
+connection.close()
 
 
 def urand():
